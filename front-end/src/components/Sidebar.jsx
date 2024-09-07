27cb327ef8b678a10c631516431dae3c4d385223
Hiep Thai <hiepproso01@gmail.com>
2024-09-07 21:31:18 +0700

    Ecommerce

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -1,55 +1,52 @@
 import React from 'react';
-import { BsGrid1X2Fill,BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsListCheck, BsMenuButtonWideFill, BsFillGearFill, BsCardText } from 'react-icons/bs';
-import  logo from "../img/logo.png"
+import { Link } from 'react-router-dom';
+import { BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsListCheck, BsMenuButtonWideFill, BsFillGearFill, BsCardText } from 'react-icons/bs';
+import logo from "../img/logo.png";
+
 const Sidebar = ({ openSidebarToggle }) => {
-  
   return (
     <aside
       id="sidebar"
       className={`sidebar ${openSidebarToggle ? 'shifted' : ''}`}
     >
       <div className='sidebar-header'>
-      <img src={logo} alt='Logo' className='logo' />
-        {/* <button onClick={OpenSidebar} className='md:hidden'>
-          &times;
-        </button> */}
+        <img src={logo} alt='Logo' className='logo' />
       </div>
       <ul className='sidebar-list'>
-        
         <li className='sidebar-item'>
-          <a href="/product" className='flex-center1'>
+          <Link to="/product" className='flex-center1'>
             <BsCardText className='icon' /> Quản lý sản phẩm
-          </a>
+          </Link>
         </li>
         <li className='sidebar-item'>
-          <a href="/category" className='flex-center1'>
+          <Link to="/category" className='flex-center1'>
             <BsFillArchiveFill className='icon' /> Quản lý danh mục
-          </a>
+          </Link>
         </li>
         <li className='sidebar-item'>
-          <a href="/supplier" className='flex-center1'>
+          <Link to="/supplier" className='flex-center1'>
             <BsFillGrid3X3GapFill className='icon' /> Quản lý nhà cung cấp
-          </a>
+          </Link>
         </li>
         <li className='sidebar-item'>
-          <a href="" className='flex-center1'>
+          <Link to="" className='flex-center1'>
             <BsPeopleFill className='icon' /> Quản lý khách hàng
-          </a>
+          </Link>
         </li>
         <li className='sidebar-item'>
-          <a href="" className='flex-center1'>
+          <Link to="" className='flex-center1'>
             <BsListCheck className='icon' /> Quản lý đơn hàng
-          </a>
+          </Link>
         </li>
         <li className='sidebar-item'>
-          <a href="" className='flex-center1'>
+          <Link to="" className='flex-center1'>
             <BsMenuButtonWideFill className='icon' /> Quản lý báo cáo
-          </a>
+          </Link>
         </li>
         <li className='sidebar-item'>
-          <a href="" className='flex-center1'>
+          <Link to="" className='flex-center1'>
             <BsFillGearFill className='icon' /> Cài đặt
-          </a>
+          </Link>
         </li>
       </ul>
     </aside>
